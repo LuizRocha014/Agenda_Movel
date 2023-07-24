326b49781f8af6dd7ccf56b736cc0c4b8385f107
NOTEBOOKDOROCHA\luiza <luizaugusto.clima@gmail.com>
2023-07-24 13:19:15 -0300

    Adicionado Pastas de divisão de arquivos

---
+++ Agenda_Movel/Agenda_Movel/Data/Context/DataContext.cs
@@ -0,0 +1,37 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Agenda_Movel.Data.Context
+{
+    public class DataContext
+    {
+
+        private static SQLiteConnection _sqlLiteConnection;
+
+        public static DataContext _lazy;
+
+        public static DataContext Current
+        {
+            get
+            {
+                if (_lazy == null)
+                    _lazy = new DataContext();
+
+                return _lazy;
+            }
+        }
+
+
+        public DataContext()
+        {
+            var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "database.db3");
+            _sqlLiteConnection = new SQLiteConnection(databasePath);
+        }
+
+
+        public SQLiteConnection Conexao { get { return _sqlLiteConnection; } set { _sqlLiteConnection = value; } }
+    }
+}
