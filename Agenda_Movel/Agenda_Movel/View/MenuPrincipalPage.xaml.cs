a1b8e1c509e37bb892accc2c6801ba11c254b4f8
NOTEBOOKDOROCHA\luiza <luizaugusto.clima@gmail.com>
2023-08-02 22:02:36 -0300

    Criação de tela de Menu principal com menu, adicionado imagens

---
+++ Agenda_Movel/Agenda_Movel/View/MenuPrincipalPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Agenda_Movel.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPrincipalPage : ContentPage
+    {
+        public MenuPrincipalPage()
+        {
+            InitializeComponent();
+            List<string> list = new List<string>();
+            list.Add("Home");
+            list.Add("Profile");
+            list.Add("Inbox");
+            list.Add("Outbox");
+            list.Add("Sent");
+            list.Add("Draft");
+            listView.ItemsSource = list;
+        }
+        void hamburgerButton_Clicked(object sender, EventArgs e)
+        {
+            navigationDrawer.ToggleDrawer();
+        }
+
+        private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            // Your codes here
+            navigationDrawer.ToggleDrawer();
+        }
+    }
+}
